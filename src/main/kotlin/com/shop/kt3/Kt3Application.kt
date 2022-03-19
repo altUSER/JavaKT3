fd11b54155e787affcd5ba9628fb9d92a8e84e6d
altUSER <blaksteam@yandex.ru>
2022-03-19 12:54:58 +0500

    Initial commit

---
+++ src/main/kotlin/com/shop/kt3/Kt3Application.kt
@@ -0,0 +1,11 @@
+package com.shop.kt3
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Kt3Application
+
+fun main(args: Array<String>) {
+	runApplication<Kt3Application>(*args)
+}
---
+++ src/test/kotlin/com/shop/kt3/Kt3ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.shop.kt3
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Kt3ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
